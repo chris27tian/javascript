dcb99589045c2098b468d5543ee790a1ec61836d
chris27tian <chris27tian@gmail.com>
2020-05-17 20:41:19 +0200

    methode

--- hello.js
+++ hello.js
@@ -123,10 +123,74 @@ function direBonjour() {
   
   console.log(direBonjour()); // "Bonjour !"
   console.log(message); // Erreur : la variable message n'existe pas ici
-//fonctions anonymes
+
+  //fonctions anonymes
   const bonjour = function(prenom) {
     const message = `Bonjour, ${prenom} !`;
     return message;
   }
   
   console.log(bonjour("Thomas")); // "Bonjour, Thomas !"
+
+// fonction fleche
+const bonjour = (prenom) => {
+    const message = `Bonjour, ${prenom} !`;
+    return message;
+  }
+  
+  console.log(bonjour("Thomas")); // "Bonjour, Thomas !"
+
+  // Dificile de faire plus concis !
+const bonjour = prenom => `Bonjour, ${prenom} !`;
+
+console.log(bonjour("Thomas")); // "Bonjour, Thomas !"
+
+// POO
+const stylo = {
+    type: "bille",
+    couleur: "bleu",
+    marque: "Bic"
+  };
+  
+  // Modification de la propriété "couleur"
+  stylo.couleur = "rouge";
+  
+  // "J'écris avec un stylo bille rouge de marque Bic"
+  console.log(`J'écris avec un stylo ${stylo.type} ${stylo.couleur} de marque ${stylo.marque}`);
+
+
+// aurora
+const aurora = {
+    nom: "Aurora",
+    sante: 150,
+    force: 25
+  };
+  
+  // "Aurora a 150 points de vie et 25 en force"
+  console.log(`${aurora.nom} a ${aurora.sante} points de vie et ${aurora.force} en force`);
+  
+  console.log("Aurora est blessée par une flèche");
+  aurora.sante = aurora.sante - 20;
+  
+  console.log("Aurora trouve un bracelet de force");
+  aurora.force = aurora.force + 10;
+  
+  // "Aurora a 130 points de vie et 35 en force"
+  console.log(`${aurora.nom} a ${aurora.sante} points de vie et ${aurora.force} en force`);
+
+  // methode 
+
+  const aurora = {
+    nom: "Aurora",
+    sante: 150,
+    force: 25,
+  
+    // Renvoie la description du personnage
+    decrire() {
+      return `${this.nom} a ${this.sante} points de vie et ${this.force} en force`;
+    }
+  };
+  
+  // "Aurora a 150 points de vie et 25 en force"
+  console.log(aurora.decrire());
+  
