159b88ff3eba41e32228be33b69915438bfd200d
chris27tian <chris27tian@gmail.com>
2020-05-17 10:34:10 +0200

    constantes

--- hello.js
+++ hello.js
@@ -1,3 +1,7 @@
 console.log("Bonjour en JavaScript !");
 let a;
+a = 3.14;
 console.log(a);
+const a = 3.14; // Création d'une variable constanteconst a = 3.14; // Création d'une variable constante
+a = 6.28;       // Impossible : a ne peut pas changer de valeur !
+a = 6.28;       // Impossible : a ne peut pas changer de valeur !
