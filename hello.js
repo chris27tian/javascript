f2cd262f691e4c0b502f641d6e7e6e802d676dac
chris27tian <chris27tian@gmail.com>
2020-05-17 17:03:51 +0200

    suite de l'exercice suite1

--- hello.js
+++ hello.js
@@ -41,3 +41,56 @@ const nombre = Number(prompt("Entrez un nombre :"));
 if (nombre > 0) {
   console.log(nombre + " est positif");
 }
+
+// instrcution else
+const nombre = Number(prompt("Entrez un nombre :"));
+if (nombre > 0) {
+  console.log(nombre + " est positif");
+}
+else {
+  console.log(nombre + " est négatif ou nul");
+}
+
+// condiiton imbriqué
+const nombre = Number(prompt("Entrez un nombre :"));
+if (nombre > 0) {
+  console.log(nombre + " est positif");
+} else { // nombre <= 0
+  if (nombre < 0) {
+    console.log(nombre + " est négatif");
+  } else { // nombre === 0
+    console.log(nombre + " est nul");
+  }
+}
+
+// exprimer un choix
+const meteo = prompt("Quel temps fait-il dehors ?");
+if (meteo === "soleil") {
+  console.log("Sortez en t-shirt.");
+} else if (meteo === "vent") {
+  console.log("Sortez en pull.");
+} else if (meteo === "pluie") {
+  console.log("Sortez en blouson.");
+} else if (meteo === "neige") {
+  console.log("Restez au chaud à la maison.");
+} else {
+  console.log("Je n'ai pas compris !");
+}
+
+// boucle while
+console.log("Début du programme");
+let nombre = 1;
+while (nombre <= 20) {
+  console.log(nombre);
+  nombre++;
+}
+console.log("Fin du programme");
+
+
+//boucle for
+for (let i = 1; i <= 5; i++) {
+    console.log(i); // OK
+}
+
+
+console.log(i); // Erreur : la variable i n'est pas visible ici
